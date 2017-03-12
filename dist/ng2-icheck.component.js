0e8e7bd96776625113567f1aa06b30e7e40888ec
Tommaso Marchionni <tommaso.marchionni@gmail.com>
2017-03-12 12:08:19 +0100

    fix bugs

--- dist/ng2-icheck.component.js
+++ dist/ng2-icheck.component.js
@@ -6,7 +6,13 @@ var ICheckComponent = (function () {
     ICheckComponent.decorators = [
         { type: core_1.Component, args: [{
                     selector: '[iCheck]',
-                    templateUrl: './ng2-icheck.html',
+                    template: "\n        <!--<div (click)=\"isChecked = !isChecked;\" class=\"ng2-icheck\">-->\n            <!--<div [class.checked]=\"isChecked\"></div>-->\n            <!--&lt;!&ndash;<ng-content class=\"label\"></ng-content>&ndash;&gt;-->\n        <!--</div>-->\n        <div>ciao</div>\n    ",
+                    styles: [
+                        "\n            :host {\n                font-family: \"Consolas\", \"Microsoft YaHei\", Arial, arial, sans-serif;\n                overflow: hidden;\n            }\n        ",
+                        "\n            :host .checkbox {\n                width: 22px;\n                height: 22px;\n                display: inline-block;\n                vertical-align: middle;\n                background: url('ng2-icheck.png') no-repeat left;\n            }\n        ",
+                        "\n            :host .checkbox.checked {\n              background-position: -48px 0;\n            }\n        ",
+                        "\n            :host .label {\n                display: inline-block;\n                vertical-align: middle;\n            }\n        ",
+                    ]
                 },] },
     ];
     /** @nocollapse */
